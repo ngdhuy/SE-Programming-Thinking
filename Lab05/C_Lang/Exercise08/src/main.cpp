7194146275b40886970eb8eff0e860d937480618
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2020-07-24 09:13:09 +0700

    [basic] string - c/c++

--- Lab05/C_Lang/Exercise08/src/main.cpp
+++ Lab05/C_Lang/Exercise08/src/main.cpp
@@ -12,7 +12,7 @@
 int main()
 {
     char s[MAX] = "   people expect     macOS apps to be intuitive    , while simultaneously adaptable to their workflow through customization and flexibility   .Many apps offer configurable preferences, customizable interfaces, and alternate ways of completing tasks   .      windows can often be resized and interface elements hidden or repositioned. tasks can be initiated from toolbars   ,    menus     ,      controls    , keyboard    shortcuts, the Touch Bar,    accessibility   features, and more. a flexible app facilitates learning through     discoverability    .    ";
-    
+    int numCharInline = 30;
     // printf("Enter the first string: ");
     // scanf("%[^\n]s", s);
 
@@ -22,7 +22,7 @@ int main()
     
     while(i >= 0)
     {
-        if(s[i] == ' ' && (i == 0 || i == length - 1 || s[i - 1] == ' '))
+        if(s[i] == ' ' && (i == 0 || i == length - 1 || s[i - 1] == ' ' || s[i + 1] == ',' || s[i + 1] == '.' || s[i + 1] == ';' || s[i + 1] == '!' || s[i + 1] == '?' || s[i + 1] == ':'))
         {
             for(int j = i; j <= length; j++)
             {
@@ -34,24 +34,6 @@ int main()
     }
     
     printf("\nTrim string:%s!\n", s);
-
-    // Remove blank before period
-    length = strlen(s);
-    i = length;
-    while(i >= 0)
-    {
-        if(s[i] == ' ' && (s[i + 1] == ',' || s[i + 1] == '.' || s[i + 1] == ';' || s[i + 1] == '!' || s[i + 1] == '?' || s[i + 1] == ':'))
-        {
-            for(int j = i; j <= length; j++)
-            {
-                s[j] = s[j + 1];
-            }
-            length--;
-        }
-        i--;
-    }
-
-    printf("\nTrim period:%s!\n", s);
 
     // Insert blank after period
     length = strlen(s);
@@ -88,6 +70,35 @@ int main()
         s[0] -= 32;
 
     printf("\nUpper string:%s!\n", s);
+
+    length = strlen(s);
+    int count = 0;
+    for(int i = 0; i < length; i++)
+    {
+        if(count++ >= numCharInline)
+        {
+            if(s[i] != ' ')
+            {
+                while(i > 0 && s[i] != ' ')
+                {
+                    i--;
+                }
+            }
+            
+            if(s[i] == ' ')
+                i++;
+
+            for(int j = length + 1; j > i; j--)
+            {
+                s[j] = s[j - 1];
+            }
+            s[i] = '\n';
+            length++;
+            count = 0;
+        }
+    }
+
+    printf("%s\n", s);
 
     return 0;
 }
