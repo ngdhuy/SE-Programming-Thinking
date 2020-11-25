8e6bb103a137dc0340f41e03a50816c26229e6c6
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2020-11-25 09:00:50 +0700

    [Adv] OOP - Polymorphism cont.

--- Session_02_OOP/Lab03/Exercise01/src/Dog.cpp
+++ Session_02_OOP/Lab03/Exercise01/src/Dog.cpp
@@ -5,3 +5,60 @@
  **/ 
 
 #include "Dog.h"
+
+Dog::Dog() : Animal()
+{
+    this->breed = "Nothing";
+    this->size = 0;
+}
+
+Dog::Dog(string breed, int size) : Animal()
+{
+    this->breed = breed; 
+    this->size = size;
+}
+
+Dog::Dog(int id, string name, string breed, int size) : Animal(id, name)
+{
+    this->breed = breed; 
+    this->size = size;
+}
+
+Dog::Dog(Dog& dog)
+{
+    this->breed = dog.breed;
+    this->size = dog.size;
+
+    this->setID(dog.getID());
+    this->setName(dog.getName());
+}
+
+void Dog::setBreed(string breed)
+{
+    this->breed = breed;
+}
+
+void Dog::setSize(int size)
+{
+    this->size = size;
+}
+
+string Dog::getBreed()
+{
+    return this->breed;
+}
+
+int Dog::getSize()
+{
+    return this->size;
+}
+
+void Dog::Run()
+{
+    cout << "Dog is Running" << endl;
+}
+
+void Dog::MakeSound()
+{
+    cout << "Gau gau" << endl;
+}
--- Session_02_OOP/Lab03/Exercise01/src/main.cpp
+++ Session_02_OOP/Lab03/Exercise01/src/main.cpp
@@ -8,6 +8,9 @@
 
 #include "Animal.h"
 #include "Cat.h"
+#include "Dog.h"
+
+#define MAX 10
 
 using namespace std;
 
@@ -17,8 +20,19 @@ int main()
     cout << cat->getID() << "-" << cat->getName() << "-" << cat->getColor() << endl;
 
     cat->Run();
-
     cat->MakeSound();   
+
+    cout << endl << "----------------------" << endl;
+
+    Animal* arr[MAX];
+    int n = 3; 
+
+    arr[0] = new Dog();
+    arr[1] = new Cat(2, "Meo", "White");
+    arr[2] = new Dog(3, "Cho", "Corgi", 1);
+
+    for(int i = 0; i < n; i++)
+        arr[i]->MakeSound();
     
     return 0;
 }
